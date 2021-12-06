fb97cc073956dfcbdfe84d91682ebb7131f27910
Anton Teremshonok <a.teremshonok@biopicmedical.com>
2021-12-06 19:25:21 +0700

    fix bugs4

--- adduser.py
+++ adduser.py
@@ -12,16 +12,15 @@ def get_script_dir(follow_symlinks=True):
     if follow_symlinks:
         path = os.path.realpath(path)
     return os.path.dirname(path)
-#print(get_script_dir())
+#print(get_script_dir()+'/users.db')
 
+userdb = os.path.join(get_script_dir(), 'users.db')
 in_user = sys.argv[1]
 in_password = sys.argv[2]
 in_password = hashlib.sha256(in_password.encode("utf-8")).hexdigest()
-f = open(get_script_dir()+'/users.db', 'w+')
-
+f = open(userdb, 'a+')
 new_line = in_user + ":" + in_password
-
-if os.path.getsize("users.db") > 0:
+if os.path.getsize(userdb) > 0:
     f.write("\n" + new_line)
     f.close()
     sys.exit(0)
