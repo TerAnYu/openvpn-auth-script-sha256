9d10316bc82de0e8ab081c541444fdb037a74608
Anton Teremshonok <a.teremshonok@biopicmedical.com>
2021-12-06 19:02:51 +0700

    fix bugs

--- adduser.py
+++ adduser.py
@@ -2,14 +2,30 @@
 import sys
 import hashlib
 import os
+import inspect
+
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+#print(get_script_dir())
 
 in_user = sys.argv[1]
 in_password = sys.argv[2]
 in_password = hashlib.sha256(in_password.encode("utf-8")).hexdigest()
-f = open('users.db', 'w+')
+f = open(get_script_dir()+'/users.db', 'w+')
+
 new_line = in_user + ":" + in_password
+
 if os.path.getsize("users.db") > 0:
     f.write("\n" + new_line)
+	f.close()
+	sys.exit(0)
 else:
     f.write(new_line)
-f.close()
+	f.close()
+	sys.exit(0)
--- auth.py
+++ auth.py
@@ -1,13 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 import sys
 import hashlib
+import os
+import inspect
+
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+#print(get_script_dir())
 
 tmpFile = open(sys.argv[1], 'r')
 lines = tmpFile.readlines()
 input_user = lines[0].strip()
 input_pass = lines[1].strip().encode("utf-8")
 input_pass = hashlib.sha256(input_pass).hexdigest()
-f = open('users.db', 'r')
+f = open(get_script_dir()+'/users.db', 'r')
 for line in f:
     line = line.strip()
     array = line.split(":")
@@ -16,6 +28,8 @@ for line in f:
     if user == input_user:
         if password == input_pass:
             print("auth success! ")
+            f.close()
             sys.exit(0)
 print("epic fail")
+f.close()
 sys.exit(1)
